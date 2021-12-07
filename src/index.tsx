20bf35686a22bbd23d97b6df2394c8c7839a43db
pan9killer <pan9killer@gmail.com>
2021-12-07 12:36:19 +0400

    tests working

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom";
 import "./index.module.scss";
 import App from "./components/App";
 
-const aaa = [1, 2, 3];
+const asaa = [1, 2, 3];
 const bbb = [1, 2, 3];
 const ccs = [1, 2, 3];
 
