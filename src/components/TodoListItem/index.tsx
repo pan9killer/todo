2f365b5e08c7212efad9dd0408e361124985b5f4
pan9killer <pan9killer@gmail.com>
2021-12-07 12:40:25 +0400

    test working

--- src/components/TodoListItem/index.tsx
+++ src/components/TodoListItem/index.tsx
@@ -1,5 +1,8 @@
 // import styles from "./index.module.scss";
 
+const asaa = [1, 2, 3];
+const asza = [1, 2, 3];
+
 const TodoListItem = () => {
   return <div>TodoListItem</div>;
 };
--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,8 @@ import ReactDOM from "react-dom";
 import "./index.module.scss";
 import App from "./components/App";
 
+const asaa = [1, 2, 3];
+const bbb = [1, 2, 3];
+const ccs = [1, 2, 3];
+
 ReactDOM.render(<App />, document.getElementById("root"));
