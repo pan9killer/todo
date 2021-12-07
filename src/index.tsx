0d935e240425f0f33a0398e6a6081cf827f44755
pan9killer <pan9killer@gmail.com>
2021-12-07 12:28:58 +0400

    tests working

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
-import ReactDOM from 'react-dom';
-import './index.module.scss';
-import App from './components/App';
+import ReactDOM from "react-dom";
+import "./index.module.scss";
+import App from "./components/App";
 
+const aaa = [1, 2, 3];
+const bbb = [1, 2, 3];
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,7 +1,5 @@
 import { ADD_TODO, REMOVE_TODO } from "./constants";
 
-const aaa = [1, 2, 3];
-
 export const addNewItem = (payload: any) => ({ type: ADD_TODO, payload });
 export const removeSelectedItem = (payload: any) => ({
   type: REMOVE_TODO,
