c850af7246676472a07834faa968f2ce3ea1fc5c
pan9killer <pan9killer@gmail.com>
2021-12-07 13:09:12 +0400

    testas

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,6 @@ import "./index.module.scss";
 import App from "./components/App";
 const ax = "ds";
 const asssa = "ds";
+const aszsa = "ds";
+const aszsas = "ds";
 ReactDOM.render(<App />, document.getElementById("root"));
