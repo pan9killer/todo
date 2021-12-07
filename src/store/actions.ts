9ad6b019635db6984aa29037fc238299324f0691
pan9killer <pan9killer@gmail.com>
2021-12-07 13:02:21 +0400

    testing test

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,5 +1,5 @@
 import { ADD_TODO, REMOVE_TODO } from "./constants";
-
+const as = 12;
 export const addNewItem = (payload: any) => ({ type: ADD_TODO, payload });
 export const removeSelectedItem = (payload: any) => ({
   type: REMOVE_TODO,
