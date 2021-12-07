8b2cc50a552a3c49535c066306ac7ac941f4c93e
pan9killer <pan9killer@gmail.com>
2021-12-07 12:48:11 +0400

    testing tests

--- src/components/TodoListItem/index.tsx
+++ src/components/TodoListItem/index.tsx
@@ -1,5 +1,5 @@
 // import styles from "./index.module.scss";
-
+const ssa = "dsads";
 const TodoListItem = () => {
   return <div>TodoListItem</div>;
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom";
 import "./index.module.scss";
 import App from "./components/App";
-
+const ax = "ds";
 ReactDOM.render(<App />, document.getElementById("root"));
