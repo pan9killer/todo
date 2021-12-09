28e38018fa75e2f895cbcae981571bbf5721f576
pan9killer <pan9killer@gmail.com>
2021-12-09 13:50:51 +0400

    with index in key

--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -1,27 +1,22 @@
 import { useSelector } from "react-redux";
-import { useState, useEffect } from "react";
 import TodoListItem from "../TodoListItem";
 import styles from "./index.module.scss";
 import { IState } from "../../interfaces/interfaces";
 
 const TodoList = () => {
-  const [elements, setElements] = useState([<></>]);
   const state = useSelector((state: IState) => state);
 
-  useEffect(() => {
-    const element = state.todoData.map((item: string, index: number) => {
-      const newI = Math.floor(Math.random() * 100);
-      console.log(newI);
-      return (
-        <li key={newI} className={styles.list__group__item}>
-          <TodoListItem label={item} />
-        </li>
-      );
-    });
-    setElements(element);
-  }, [state.todoData]);
-
-  return <ul className={styles.list__group}>{elements}</ul>;
+  return (
+    <ul className={styles.list__group}>
+      {state.todoData.map((item: string, index: number) => {
+        return (
+          <li key={index + 1} className={styles.list__group__item}>
+            <TodoListItem label={item} />
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default TodoList;
